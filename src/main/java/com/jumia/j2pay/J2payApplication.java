fa961f2f85592f146a3bb6a7203b97f8e5815069
AlexMaina <maina.alex2010@gmail.com>
2022-01-17 19:12:26 +0300

    Adds Sqllite configs

---
+++ src/main/java/com/jumia/j2pay/J2payApplication.java
@@ -0,0 +1,13 @@
+package com.jumia.j2pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2payApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2payApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumia/j2pay/J2payApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumia.j2pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2payApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
